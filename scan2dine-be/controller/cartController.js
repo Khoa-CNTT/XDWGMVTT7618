775565218e9ba55bd6cc5569b3c0fc3e11e1b6de
Ghost Dev <ntrongquy2003@gmail.com>
2025-04-22 15:19:48 +0700

    delete all cartdetail in cart

--- scan2dine-be/controller/cartController.js
+++ scan2dine-be/controller/cartController.js
@@ -1,35 +1,100 @@
-const {Cart, Customer, Product} = require('../model/model');
+const { Cart, Customer, Product, CartDetail } = require('../model/model');
+const { creatCart } = require('../service/cartService');
 
-// hello 
 const cartController = {
     // add a cart
-    addCart:  async (req,res) => {
+    addCart: async (req, res) => {
         try {
-            const newCart = new Cart(req.body);
-            const saveCart = await newCart.save();
+            const newCart = await creatCart(req.body);
             // req.body.customer: laays id cua customer
-            if (req.body.customer){ 
+            if (req.body.customer) {
                 const customer = await Customer.findById(req.body.customer);
-                await customer.updateOne({$push: {
-                    cart: saveCart._id
-                }})
+                await customer.updateOne({
+                    $push: {
+                        cart: saveCart._id
+                    }
+                })
             };
-            res.status(200).json(saveCart);
+            return res.status(200).json(newCart);
         } catch (error) {
-            res.status(500).json(error);
+            console.error("Error in:", error);
+            return res.status(500).json({ message: "Server error", error: error.message || error });
         }
     },
     // get cart
-    getCart: async(req,res) =>{
+    getCart: async (req, res) => {
         try {
-            const cart = await Cart.find().populate({path:"customer", select:"name"});
-            res.status(200).json(cart);
+            const cart = await Cart.find().populate({ path: "customer", select: "name" });
+            return res.status(200).json(cart);
         } catch (error) {
-            res.status(500).json(error);
+            return res.status(500).json(error);
         }
     },
 
-    // delete cart
+    //delete all cartdetail theo id của giỏ hàng
+    // deleteCartdetail: async (req, res) => {
+    //     try {
+    //         const deleteAllcartdetail = await Cart.findById(req.params.id);
+
+    //         if (!deleteAllcartdetail) {
+    //             return res.status(404).json({ message: "not found" })
+    //         }
+    //         if (deleteAllcartdetail) {
+    //             await CartDetail.deleteMany({ cart: req.params.id })
+    //         }
+
+    //         await Cart.findByIdAndUpdate(deleteAllcartdetail._id, {
+    //             $set: {
+    //                 cartdetail: []
+    //             }
+    //         })
+
+    //         return res.status(200).json({
+    //             message: "Cart and related deleted successfully",
+    //             delete: deleteAllcartdetail
+    //         })
+    //     } catch (error) {
+    //         console.error("Error in addCartdetail:", error);
+    //         return res.status(500).json({ message: "Server error", error: error.message || error });
+    //     }
+    // },
+    deleteCartdetail: async (req, res) => {
+        try {
+            const cart = await Cart.findById(req.params.id);
+
+            if (!cart) {
+                return res.status(404).json({ message: "Cart not found" });
+            }
+
+            // Lấy danh sách CartDetail liên quan tới cart
+            const relatedCartDetails = await CartDetail.find({ cart: cart._id });
+            const cartDetailIds = relatedCartDetails.map(cd => cd._id);
+
+            // Xoá các CartDetail liên quan đến cart
+            await CartDetail.deleteMany({ cart: cart._id });
+
+            // Gỡ CartDetail khỏi Product
+            await Product.updateMany(
+                { cartdetail: { $in: cartDetailIds } },
+                { $pull: { cartdetail: { $in: cartDetailIds } } }
+            );
+
+            // Xoá mảng cartdetail trong Cart nếu có
+            await Cart.findByIdAndUpdate(cart._id, {
+                $set: { cartdetail: [] }
+            });
+
+            return res.status(200).json({
+                message: "CartDetails deleted and removed from Product successfully",
+                deletedCartDetailIds: cartDetailIds
+            });
+
+        } catch (error) {
+            console.error("Error in deleteCartdetail:", error);
+            return res.status(500).json({ message: "Server error", error: error.message || error });
+        }
+    }
+
 }
 
 module.exports = cartController;
--- scan2dine-be/routes/cart.js
+++ scan2dine-be/routes/cart.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 const cartController = require("../controller/cartController");
 // add a cart
-router.post("/",cartController.addCart);
+// router.post("/",cartController.addCart);
 
 // show all 
 router.get('/', cartController.getCart);
+router.delete('/:id', cartController.deleteCartdetail);
 
 
 module.exports =router;
