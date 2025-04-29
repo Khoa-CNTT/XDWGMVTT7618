7145ebca88765d28d6b4d63fea1c593734c58473
Ghost Dev <ntrongquy2003@gmail.com>
2025-04-29 21:46:24 +0700

    done task 29/4

--- scan2dine-be/controller/cartController.js
+++ scan2dine-be/controller/cartController.js
@@ -82,7 +82,7 @@ const cartController = {
             if (cartDetails.length === 0) return res.status(400).json({ message: 'Giỏ hàng trống!' });
 
             let existingOrder = await Order.findOne({
-                customer: customerId,
+                table: table,
                 od_status: '2'
             });
 
