b322fcb9508fafb801889959df6f8b96c573351e
Ghost Dev <ntrongquy2003@gmail.com>
2025-05-08 13:34:36 +0700

    confirm status orderdetail router

--- scan2dine-be/routes/orderdetail.js
+++ scan2dine-be/routes/orderdetail.js
@@ -9,4 +9,10 @@ router.delete('/', orderdetailCOntroller.downQuantity);
 router.patch('/:id', orderdetailCOntroller.updateOrderdetial); // Sử dụng phương thức PUT cho updat
 // cập nhật trạng thái chi tiết đơn hàng
 router.patch('/newStatus/:orderdetail', orderdetailCOntroller.updateOrderdetailStatus); 
+// cập nhật trạng thái orderdetail xác nhận -> đang chuẩn bị
+// req.params : id orderdetail - req.body: id order , newStatus
+router.patch('/confirm/prepare/:orderdetail', orderdetailCOntroller.confirmStatus3); 
+// cập nhật trạng thái orderdetail đang chuẩn bị -> hoàn thành
+// req.params : id orderdetail - req.body: id order , newStatus
+router.patch('/confirm/complete/:orderdetail', orderdetailCOntroller.confirmStatusComplete); 
 module.exports = router;
