8583845588c20abe7b0ef3d3baffba5d5af3fbfa
Ghost Dev <ntrongquy2003@gmail.com>
2025-05-22 23:53:19 +0700

    get username

--- scan2dine-be/controller/userController.js
+++ scan2dine-be/controller/userController.js
@@ -20,6 +20,23 @@ const userController = {
             res.status(500).json({ message: 'Internal server error' });
         }
     },
+    // get user
+    getAllUserName : async (req, res) => {
+        try {
+            // const users = await User.find().select('username');
+            const users = await User.find();
+            if (!users || users.length === 0) {
+                return res.status(404).json({ message: 'No users found' });
+            }
+
+            const userNames = users.map(user => user.username);
+            res.status(200).json(userNames);
+            // res.status(200).json(users);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
 
     // update user
     updateUser: async (req, res) => {
